f599664353acb145acf8a851b1cdd116b07a7d7f
Arman Yaraee <arman@yaraee.net>
2016-09-15 18:45:44 +0100

    Added Additional assets

---
+++ Assets/GenericExtensions/PrimeTypeExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace GenericExtensions
+{
+    public static class PrimeTypeExtensions
+    {
+        public static int Direction(this float val)
+        {
+            return val == 0
+                ? 0
+                : val > 0
+                    ? 1
+                    : -1;
+        }
+    }
+}
---
+++ Assets/_Scripts/Behaviours/BoundParticles.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using UnityEngine;
+using Zenject;
+using _Scripts.Definitions;
+
+namespace _Scripts.Behaviours
+{
+    [RequireComponent(typeof(ParticleSystem))]
+    public class BoundParticles : MonoBehaviour
+    {
+        public float MaxHeight;
+
+        private ParticleSystem _ps;
+
+        [Inject]
+        public void Initialize(Settings settings)
+        {
+            _ps = GetComponent<ParticleSystem>();
+        }
+
+        void Update()
+        {
+            ParticleSystem.Particle[] particles = new ParticleSystem.Particle[_ps.particleCount];
+
+            int num = _ps.GetParticles(particles);
+
+            while (--num >= 0)
+            {
+                if (particles[num].position.z > MaxHeight)
+                {
+                    particles[num].lifetime = 0;
+                }
+            }
+
+            _ps.SetParticles(particles, particles.Length);
+        }
+
+
+
+    }
+}
