43fe478ed87a9a6623b0c80fda469ddc59c90f45
Arman Yaraee <arman@yaraee.net>
2016-09-16 21:28:06 +0100

    Added Debugging and obstacle masses affect their speed

---
+++ Assets/_Scripts/Debugging/DebugManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+using _Scripts.Definitions.ConstantClasses;
+
+#if UNITY_EDITOR
+namespace _Scripts.Debugging
+{
+    public partial class DebugManager : MonoBehaviour
+    {
+        private Dictionary<string, Text> _texts;
+
+        [Inject]
+        void Initialize()
+        {
+            var texts = GetComponentsInChildren<Text>();
+
+            _texts = new Dictionary<string, Text>();
+
+            foreach (var text in texts)
+            {
+                _texts[text.name] = text;
+            }
+        }
+
+        void Update()
+        {
+            SetValues();
+        }
+
+        private void SetValues()
+        {
+            _texts[Names.HighestScore].text = string.Format("Highest Score: {0}",
+                PlayerPrefs.GetInt(PlayerPrefKeys.MaxBonus));
+        }
+
+        public static class Names
+        {
+            public const string HighestScore = "HighestScore";
+        }
+    }
+}
+#endif
