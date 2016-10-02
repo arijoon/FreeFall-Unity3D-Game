6c80d4b1966a932d3ad1c887d331d8628b895a01
Arman Yaraee <arman@yaraee.net>
2016-10-02 20:53:51 +0100

    Added medals to leaderboard

---
+++ Assets/_Scripts/InjectablePrefabs/MedalsSprites.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace _Scripts.InjectablePrefabs
+{
+    public class MedalsSprites: MonoBehaviour
+    {
+        [SerializeField]
+        private Sprite[] medals;
+
+        public Sprite[] Medals
+        {
+            get { return medals; }
+        }
+    }
+}
--- Assets/_Scripts/Ui/LeaderBoardRow.cs
+++ Assets/_Scripts/Ui/LeaderBoardRow.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using Zenject;
 using _Scripts.Backend.Models;
+using _Scripts.InjectablePrefabs;
 
 namespace _Scripts.Ui
 {
@@ -9,12 +11,29 @@ namespace _Scripts.Ui
         public Text Rank;
         public Text Username;
         public Text Score;
+        public Image MedalImage;
+
+        private MedalsSprites _medalsSprites;
+
+        [Inject]
+        public void Initialize(MedalsSprites medalsSprites)
+        {
+            _medalsSprites = medalsSprites;
+        }
 
         public void SetData(LeaderBoardUser user)
         {
             Rank.text = user.Rank.HasValue ? user.Rank.ToString() : "N/A";
             Username.text = user.DisplayName;
             Score.text = user.Score;
+
+            if (user.Rank.HasValue && user.Rank.Value <= _medalsSprites.Medals.Length)
+            {
+                Sprite sprite = _medalsSprites.Medals[user.Rank.Value - 1];
+
+                MedalImage.sprite = sprite;
+                MedalImage.gameObject.SetActive(true);
+            }
         }
 
         public void SetTextColor(Color newColor)
