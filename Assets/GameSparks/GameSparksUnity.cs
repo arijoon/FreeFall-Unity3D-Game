4101fc080df977c5a75afc23b4f899bf009d68b0
Arman Yaraee <arman@yaraee.net>
2016-10-08 20:56:15 +0100

    Fixed iOS issues and empty files

--- Assets/GameSparks/GameSparksUnity.cs
+++ Assets/GameSparks/GameSparksUnity.cs
@@ -1,3 +1,5 @@
+using System;
+using GameSparks;
 using UnityEngine;
 using GameSparks.Platforms;
 
--- Assets/Tune/Scripts/TuneExterns.cs
+++ Assets/Tune/Scripts/TuneExterns.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Runtime.InteropServices;
+
 namespace TuneSDK
 {
     // Externs used by the iOS component.
